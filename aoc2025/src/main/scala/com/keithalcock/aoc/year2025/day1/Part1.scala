17d3b7811bc5e0efd6f50bdd7a2fe2e599ba878f
Keith Alcock <github@keithalcock.com>
2026-03-09 20:53:27 -0700

    Add Day1, Part1

--- aoc2025/src/main/scala/com/keithalcock/aoc/year2025/day1/Part1.scala
+++ aoc2025/src/main/scala/com/keithalcock/aoc/year2025/day1/Part1.scala
@@ -1,21 +1,51 @@
 package com.keithalcock.aoc.year2025.day1
 
-import com.keithalcock.aoc.year2025.Aoc
+import scala.io.Source
+import scala.util.Using
 
-object Part1 extends Aoc[Int]:
+case class Rotation(value: Int):
 
-  def run(lines: Iterator[String]): Int =
-    val depths = lines
-        .map(_.toInt)
-    val increasingCount = depths
-        .sliding(2)
-        .count:
-          case Seq(prev, next) => prev < next
+  def this(size: Int, direction: Char, amount: Int) = {
+    this {
+      val value = direction match
+        case 'L' => amount % size
+        case 'R' => (-(amount % size) + size) % size
+        case _ => ???
 
-    increasingCount
+      require(amount >= 0)
+      assert(0 <= value && value < size)
+      value
+    }
+  }
+
+  override def toString(): String =
+    if value >= 0 then
+      s"L$value"
+    else
+      s"R${value.abs}"
+
+object Part1:
+  val size = 100
+
+  def run(start: Int, resourceName: String): Int =
+    Using.resource(Source.fromResource(resourceName)): source =>
+      run(start, source)
+
+  def run(start: Int, source: Source): Int =
+    run(start, source.getLines)
+
+  def run(start: Int, lines: Iterator[String]): Int =
+    val count = lines
+        .map: line =>
+          new Rotation(size, line.head, line.tail.toInt)
+        .scanLeft(start): (current: Int, rotation: Rotation) =>
+          (current + rotation.value) % size
+        .count(_ == 0)
+
+    count
 
 object Part1App extends App:
-  val result = Part1.run("com/keithalcock/aoc/year2025/day1/input.txt")
+  val result = Part1.run(50, "com/keithalcock/aoc/year2025/day1/input.txt")
 
   println(result)
 
--- aoc2025/src/test/scala/com/keithalcock/aoc/year2025/day1/Part1Test.scala
+++ aoc2025/src/test/scala/com/keithalcock/aoc/year2025/day1/Part1Test.scala
@@ -4,11 +4,27 @@ import com.keithalcock.aoc.Test
 
 class Part1Test extends Test {
 
+  behavior of "Rotation"
+
+  it should "behave" in {
+    val size = 100
+    val l = 'L'
+    val r = 'R'
+
+    new Rotation(size, l, 0).value should be (0)
+    new Rotation(size, l, 99).value should be (99)
+    new Rotation(size, l, 101).value should be (1)
+
+    new Rotation(size, r, 0).value should be (0)
+    new Rotation(size, r, 99).value should be (1)
+    new Rotation(size, r, 101).value should be (99)
+  }
+
   behavior of "Day1.Part1"
 
   it should "behave" in {
-    val expectedResult = 7
-    val actualResult = Part1.run("com/keithalcock/aoc/year2025/day1/test.txt")
+    val expectedResult = 3
+    val actualResult = Part1.run(50, "com/keithalcock/aoc/year2025/day1/test.txt")
 
     actualResult should be (expectedResult)
   }
