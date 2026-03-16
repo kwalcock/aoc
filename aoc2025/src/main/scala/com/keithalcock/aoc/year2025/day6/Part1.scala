72d55bd1f718559e39e698418aa455fa7446ab45
Keith Alcock <github@keithalcock.com>
2026-03-15 21:52:35 -0700

    Improve Day6, Part1

--- aoc2025/src/main/scala/com/keithalcock/aoc/year2025/day6/Part1.scala
+++ aoc2025/src/main/scala/com/keithalcock/aoc/year2025/day6/Part1.scala
@@ -16,10 +16,39 @@ object Part1 extends Aoc[Long]:
       val values = strings.init.map(_.toLong)
       val operation = strings.last
       val result = operation match
-        case "*" => values.reduce(_ * _)
-        case "+" => values.reduce(_ + _)
+        case "*" => values.product
+        case "+" => values.sum
 
       result
+    val result = results.sum
+
+    result
+
+
+
+  def run2(lines: Iterator[String]): Long =
+    val headPairs = lines.next.trim.split(raw"\s+").map: value =>
+      (value.toLong, value.toLong)
+
+    val pairs = lines.foldLeft(headPairs): (pairs, line) =>
+      val strings = line.trim.split(raw"\s+")
+
+      strings.head match
+        case "+" | "*" =>
+          pairs.zip(strings).map:
+            case (pair, string) =>
+              string match
+                case "+" => (pair._1, 0) // Keep just the sum.
+                case "*" => (0, pair._2) // Keep just the product.
+        case _ =>
+          pairs.zip(strings).map:
+            case (pair, string) =>
+              val value = string.toLong
+
+              (pair._1 + value, pair._2 * value) // Perform both calculations.
+
+    val results = pairs.map: pair =>
+      pair._1 + pair._2
     val result = results.sum
 
     result
