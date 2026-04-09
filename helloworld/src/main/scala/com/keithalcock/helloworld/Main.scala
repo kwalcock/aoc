320d1e56f9c2dfc2b20363f178ee04db53ea4a73
Keith Alcock <github@keithalcock.com>
2026-04-09 16:23:30 -0700

    Add HelloWorld

--- helloworld/src/main/scala/com/keithalcock/helloworld/Main.scala
+++ helloworld/src/main/scala/com/keithalcock/helloworld/Main.scala
@@ -1,9 +1,12 @@
 // From the helloworld/src/main/scala directory, run this program with 
 // scala-cli run com/keithalcock/helloworld/Main.scala
-
+//> using jvm 17
 //> using scala 3.3.7
 //> using platform native
 //> using nativeVersion 0.5.10
+
+// Alternatively, use mill
+
 
 package com.keithalcock.helloworld
 
